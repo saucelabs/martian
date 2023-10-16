b34dd0f90a1ce6b4da6b2ac96f3afdca9f62a83c
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-10-16 13:37:21 +0200

    martian: add isClosedConnError from http2
    This code originates from net/x/http2 and can be found in stdlib h2 server https://github.com/golang/go/blob/5873bd1d7e87e0686390d71a840a44e2e2c3e11d/src/net/http/h2_bundle.go#L4545.
    
    Windows OS error codes can be found in https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--9000-11999-.

--- proxy.go
+++ proxy.go
@@ -26,7 +26,10 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"reflect"
 	"regexp"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -42,6 +45,46 @@ import (
 var errClose = errors.New("closing connection")
 var noop = Noop("martian")
 
+func errno(v error) uintptr {
+	if rv := reflect.ValueOf(v); rv.Kind() == reflect.Uintptr {
+		return uintptr(rv.Uint())
+	}
+	return 0
+}
+
+// isClosedConnError reports whether err is an error from use of a closed network connection.
+func isClosedConnError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	// TODO: remove this string search and be more like the Windows
+	// case below. That might involve modifying the standard library
+	// to return better error types.
+	str := err.Error()
+	if strings.Contains(str, "use of closed network connection") {
+		return true
+	}
+
+	// TODO(bradfitz): x/tools/cmd/bundle doesn't really support
+	// build tags, so I can't make an http2_windows.go file with
+	// Windows-specific stuff. Fix that and move this, once we
+	// have a way to bundle this into std's net/http somehow.
+	if runtime.GOOS == "windows" {
+		if oe, ok := err.(*net.OpError); ok && oe.Op == "read" {
+			if se, ok := oe.Err.(*os.SyscallError); ok && se.Syscall == "wsarecv" {
+				const WSAECONNABORTED = 10053
+				const WSAECONNRESET = 10054
+				if n := errno(se.Err); n == WSAECONNRESET || n == WSAECONNABORTED {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+// isCloseable reports whether err is an error that indicates the client connection should be closed.
 func isCloseable(err error) bool {
 	if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
 		return true
